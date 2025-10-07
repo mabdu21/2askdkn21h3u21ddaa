227203e753f69b9515173fcf7fe0243e19983dd7
mabdu21 <woraphan88712345@gmail.com>
2025-10-08 01:41:03 +0700

    Update Free-Version-2.lua

--- Main/Webhook/Free-Version-2.lua
+++ Main/Webhook/Free-Version-2.lua
@@ -77,7 +77,7 @@ local Embed = {
     timestamp = string.format('%d-%02d-%02dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec)
 }
 
-local webhookUrl = 'https://discord.com/api/webhooks/1402082581587169361/uOSBlaQN0amnMOWup7iAzIfGqyyAPohUGxFCI8ogqiTJ3AnWMvH-VMKZPQ_LHaVgdndB'
+local webhookUrl = 'https://discord.com/api/webhooks/1402082572179345660/t9O28SP4L558pkXGT04oRCnpHhl0cflG4RG57teR72MSNN6_jEsDIMdgC0_j-xuLtqkW'
 local requestFunction = (syn and syn.request) or http_request or (http and http.request)
 
 local function loadNextScript()
