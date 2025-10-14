2035add9fc96a17e60f1c744ea742493a41bd948
mabdu21 <woraphan88712345@gmail.com>
2025-10-14 20:28:57 +0700

    Create Idk.lua

---
+++ Main/Idk.lua
@@ -0,0 +1,28 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+if not game:IsLoaded() then repeat task.wait() until game:IsLoaded() end
+
+if getgenv().RunScript == true then return end
+getgenv().RunScript = true
+
+getgenv().WebhookURL = "https://discord.com/api/webhooks/1427649302339125270/vXopOjfgOQIu6km0EMvVVd_m9W5uOtz_7xQ7HmqVj3_UdX7c_j1UtgNEJgT_NmOk79iJ"
+getgenv().AutoFarm = true
+getgenv().hop = "old"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/F99D.lua"))()
+
+local queueScript = [[
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/dyhub99night.lua"))()
+]]
+queue_on_teleport(queueScript)
+
+repeat wait(0.1) until game.Players
+local TimeWaitLoadGame = 0
+local player = game.Players.LocalPlayer
+repeat
+	wait(0.1)
+	TimeWaitLoadGame = TimeWaitLoadGame + 0.1
+	if TimeWaitLoadGame > 3 then
+		player:Kick()
+		wait(0.5)
+		game:GetService("TeleportService"):Teleport(126509999114328)
+	end
+until game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer.Character
