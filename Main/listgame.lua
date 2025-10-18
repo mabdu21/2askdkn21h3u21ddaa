ce8678bc265446869c22da82549b057fb818ae63
mabdu21 <woraphan88712345@gmail.com>
2025-10-18 17:00:05 +0700

    Create listgame.lua

---
+++ Main/listgame.lua
@@ -0,0 +1,33 @@
+return {
+    FreeVersion = {
+        allowedGamesByCreatorId = {
+            [34869880] = {
+                name = "Plants Vs Brainrots",
+                url = "https://raw.githubusercontent.com/mabdu21/kjandsaddjadbhahayenajhsjbdwa/refs/heads/main/Fuckyou.lua"
+            },
+        },
+
+        allowedGamesByPlaceId = {
+            ["6677985923"] = {
+                name = "Millionaire Empire Tycoon",
+                url = "https://raw.githubusercontent.com/mabdu21/kjandsaddjadbhahayenajhsjbdwa/refs/heads/main/MET.lua"
+            },
+        },
+    },
+
+    PremiumVersion = {
+        allowedGamesByCreatorId = {
+            [660479131] = {
+                name = "Fish a Brainrot",
+                url = "https://raw.githubusercontent.com/mabdu21/kjandsaddjadbhahayenajhsjbdwa/refs/heads/main/FaB.lua"
+            },
+        },
+
+        allowedGamesByPlaceId = {
+            ["2413927524"] = {
+                name = "The Rake REMASTERED",
+                url = "https://raw.githubusercontent.com/mabdu21/kjandsaddjadbhahayenajhsjbdwa/refs/heads/main/Kuyyyyyhahah.lua"
+            },
+        },
+    }
+}
