9962fe9b3c8c0f4a21604000a6e1b6d0926609f6
mabdu21 <woraphan88712345@gmail.com>
2025-12-29 14:14:52 +0700

    Update listpremium.lua with new list identifier

--- Main/Premium/listpremium.lua
+++ Main/Premium/listpremium.lua
@@ -1,4 +1,4 @@
--- // NEW LIST | GV-275
+-- // NEW LIST | GV-276
 return {
     -- ====================== Admin ================================
     ["Yolmar_43"] = { Tag = "dyumraisgoodguy", Time = nil, Day = 99999999, Key = "DYHUB-OWNER" },
@@ -150,7 +150,8 @@ return {
     ["Pipahaha13"] = { Tag = "electitous142", Time = nil, Day = 39, Key = "DYHUB-E4Q5W-H7NA4-KEY38DAY" },
     ["WindShootCSGO"] = { Tag = "doquagningg", Time = nil, Day = 39, Key = "DYHUB-A4CQ5-H7NA4-KEY38DAY" },
     ["kikoman714"] = { Tag = "fanluser", Time = nil, Day = 39, Key = "DYHUB-P9MA6-N6HQ5-KEY38DAY" },
-    
+
+    ["christopherbumitokfc"] = { Tag = "hailsin", Time = nil, Day = 14, Key = "DYHUB-T9PA6-N6HQ5-GIVEAWAY" },
     ["ytalo1500"] = { Tag = "ytalo__", Time = nil, Day = 14, Key = "DYHUB-T9MA6-N6HQ5-GIVEAWAY" },
     ["justinrCIJ8269"] = { Tag = "keiii0906", Time = nil, Day = 14, Key = "DYHUB-K9MA6-N6HQ5-GIVEAWAY" },
     ["Parbal_god"] = { Tag = "gorill_guy", Time = nil, Day = 30, Key = "DYHUB-H9MA6-N6HQ5-GIVEAWAY" },
