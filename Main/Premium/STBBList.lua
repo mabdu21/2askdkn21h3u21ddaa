ae9d322d5b45bfbd0c06ab7054c47b0f637ec312
mabdu21 <woraphan88712345@gmail.com>
2026-05-02 17:28:02 +0700

    Refactor user entries in STBBList.lua
    Removed Time and Day fields from user entries.

--- Main/Premium/STBBList.lua
+++ Main/Premium/STBBList.lua
@@ -1,5 +1,7 @@
 -- // NEW LIST | GV-001
 return {
     -- ====================== Admin ================================
-    ["Yolmar_43"] = { Tag = "dyumraisgoodguy", Time = nil, Day = 99999999, Key = "DYHUB-OWNER" },
+    ["Yolmar_43"] = { Tag = "dyumraisgoodguy", Key = "DYHUB-OWNER" },
+    ["kyku15567"] = { Tag = "kyku155673", Key = "DYHUB-A2E7L-STBB" },
+    
 }
