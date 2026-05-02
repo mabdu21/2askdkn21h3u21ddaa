94b7ac6473d2d6a59b4f951836b2890a4d192998
mabdu21 <woraphan88712345@gmail.com>
2026-05-02 17:29:40 +0700

    Add new user Darskay_123 to STBBList

--- Main/Premium/STBBList.lua
+++ Main/Premium/STBBList.lua
@@ -3,5 +3,5 @@ return {
     -- ====================== Admin ================================
     ["Yolmar_43"] = { Tag = "dyumraisgoodguy", Key = "DYHUB-OWNER" },
     ["kyku15567"] = { Tag = "kyku155673", Key = "DYHUB-A2E7L-STBB" },
-    
+    ["Darskay_123"] = { Tag = "darksky87", Key = "DYHUB-D3R1P-STBB-KEY14DAY" },
 }
