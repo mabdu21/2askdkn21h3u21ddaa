1b864be6fb39b9e328c512d9bfa6aa00757a061c
mabdu21 <woraphan88712345@gmail.com>
2026-05-02 17:36:06 +0700

    Update STBBList.lua with new entry for NOOB

--- Main/Premium/STBBList.lua
+++ Main/Premium/STBBList.lua
@@ -1,7 +1,7 @@
 -- // NEW LIST | GV-001
 return {
     -- ====================== Admin ================================
-    ["Yolmar_43"] = { Tag = "dyumraisgoodguy", Key = "DYHUB-OWNER" },
+    ["NOOB"] = { Tag = "dyumraisgoodguy", Key = "DYHUB-OWNER" },
     ["kyku15567"] = { Tag = "kyku155673", Key = "DYHUB-A2E7L-STBB" },
     ["Darskay_123"] = { Tag = "darksky87", Key = "DYHUB-D3R1P-STBB-KEY14DAY" },
 }
