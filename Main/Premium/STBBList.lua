3d3632fc67a73ef4740f3b6e0377bf36d401ebb2
mabdu21 <woraphan88712345@gmail.com>
2026-05-02 13:07:34 +0700

    Create STBBList.lua with admin entries
    Added a new list for admin configuration in STBBList.lua.

---
+++ Main/Premium/STBBList.lua
@@ -0,0 +1,5 @@
+-- // NEW LIST | GV-001
+return {
+    -- ====================== Admin ================================
+    ["Yolmar_43"] = { Tag = "dyumraisgoodguy", Time = nil, Day = 99999999, Key = "DYHUB-OWNER" },
+}
