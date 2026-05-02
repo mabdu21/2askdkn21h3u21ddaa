549d35c714f83426d5520880e9507676bdf883a4
mabdu21 <woraphan88712345@gmail.com>
2026-05-02 18:12:52 +0700

    Update STBBList.lua

--- Main/Premium/STBBList.lua
+++ Main/Premium/STBBList.lua
@@ -4,4 +4,5 @@ return {
     ["NOOB"] = { Tag = "dyumraisgoodguy", Key = "DYHUB-OWNER" },
     ["kyku15567"] = { Tag = "kyku155673", Key = "DYHUB-A2E7L-STBB" },
     ["Darskay_123"] = { Tag = "darksky87", Key = "DYHUB-D3R1P-STBB-KEY14DAY" },
+    ["Jelly_8king1"] = { Tag = "kadensucks5", Key = "DYHUB-E3L1P-STBB" },
 }
