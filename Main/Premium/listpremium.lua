a1d2685e1682508daf1e1666d8abafbae593962e
mabdu21 <woraphan88712345@gmail.com>
2026-06-02 03:49:14 +0700

    Update listpremium.lua

--- Main/Premium/listpremium.lua
+++ Main/Premium/listpremium.lua
@@ -1,4 +1,4 @@
--- // NEW LIST | GV-323
+-- // NEW LIST | GV-324
 return {
     -- ====================== Admin ================================
     ["Yolmar_43"] = { Tag = "dyumraisgoodguy", Time = nil, Day = 99999999, Key = "DYHUB-OWNER" },
@@ -122,7 +122,7 @@ return {
     ["zkguit10"] = { Tag = "ewertonx.", Time = "Lifetime", Day = nil, Key = "DYHUB-PW3VP-G9MHP-LIFETIME" }, -- today 2026
     ["Aiisgppdguy"] = { Tag = "aithegoat.", Time = "Lifetime", Day = nil, Key = "DYHUB-PW6VL-H9MHP-LIFETIME" }, -- today 2026
     ["samurai97397"] = { Tag = "gus008364", Time = "Lifetime", Day = nil, Key = "DYHUB-PW6VL-H9MOP-LIFETIME" }, -- today 2026
-    ["dragonfliespoi"] = { Tag = "sebbythegoat2011", Time = "Lifetime", Day = nil, Key = "DYHUB-BW8VL-HSW2P-LIFETIME" }, -- today 2026
+    ["SebbyTheGoat2011"] = { Tag = "sebbythegoat2011", Time = "Lifetime", Day = nil, Key = "DYHUB-BW8VL-HSW2P-LIFETIME" }, -- today 2026
     
     -- ====================== YOUTUBE =============================
     ["Jzyyyyyx"] = { Tag = "growwltoken", Time = "Lifetime", Day = nil, Key = "DYHUB-SINISTER" },
