73b34ce342aaeb6b1acc206366c45c97856e4ab7
mabdu21 <woraphan88712345@gmail.com>
2026-06-08 20:03:31 +0700

    Add new buyer entry for ddr532G
    Added a new entry for a trial user with a 3-day key.

--- Main/Premium/listpremium.lua
+++ Main/Premium/listpremium.lua
@@ -1,4 +1,4 @@
--- // NEW LIST | GV-331
+-- // NEW LIST | GV-332
 return {
     -- ====================== Admin ================================
     ["Yolmar_43"] = { Tag = "dyumraisgoodguy", Time = nil, Day = 99999999, Key = "DYHUB-OWNER" },
@@ -138,6 +138,7 @@ return {
   
     -- ====================== Buyer (Trial / Short-term) =============================
     ["zane76560"] = { Tag = "teguhdc1029", Time = nil, Day = 1, Key = "DYHUB-R4VA5-KEY1DAY" },
+    ["ddr532G"] = { Tag = "weare8750", Time = nil, Day = 3, Key = "DYHUB_3d_m8kZa2KFKVn9LpJ4sJHW8cPdFjB6ABNqU" },
     
     -- ====================== Booster ==============================
     ["NoraJourneys"] = { Tag = "luzzynobigl", Time = nil, Day = 14, Key = "DYHUB-2GVA5-BOOSTER" },
