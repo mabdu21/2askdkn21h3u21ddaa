035111a8539c0c1f3cbec4c60d1a38655fa57bec
mabdu21 <woraphan88712345@gmail.com>
2026-07-05 10:12:03 +0700

    Add new entry for 'tesadid1' in listpremium.lua

--- Main/Premium/listpremium.lua
+++ Main/Premium/listpremium.lua
@@ -1,4 +1,4 @@
--- // NEW LIST | GV-362
+-- // NEW LIST | GV-363
 return {
     -- ====================== Admin ================================
     ["Yolmar_43"] = { Tag = "dyumraisgoodguy", Time = nil, Day = 99999999, Key = "DYHUB-OWNER" },
@@ -142,6 +142,7 @@ return {
     ["0343_303"] = { Tag = "wralthrblx", Time = "Lifetime", Day = nil, Key = "DYHUB_lt_h3fZn2MFKBn8SpL5mHbW73cJjbtm4kVjqU" }, -- today 2026
     ["Jonnreiz"] = { Tag = "jonnreiz", Time = "Lifetime", Day = nil, Key = "DYHUB_lt_f2fRn2MFKBn3SpL4mHbW73gXdbtm4kVjqU" }, -- today 2026
     ["topmaks22856"] = { Tag = "suekiro", Time = "Lifetime", Day = nil, Key = "DYHUB_lt_e6fRu2MFKBn3SpL4mHhW53gFdbtm4kVWqU" }, -- today 2026
+    ["tesadid1"] = { Tag = "me.me87", Time = "Lifetime", Day = nil, Key = "DYHUB_lt_e4kRp2MFDBn3SpL4mHhW53yFdbtm9kVWqU" }, -- today 2026
     
     -- ====================== YOUTUBE =============================
     ["Jzyyyyyx"] = { Tag = "growwltoken", Time = "Lifetime", Day = nil, Key = "DYHUB-SINISTER" },
