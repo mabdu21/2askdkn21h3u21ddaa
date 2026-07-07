ebf1702b669b711ccda47d0ca13447b4a3d064ce
mabdu21 <woraphan88712345@gmail.com>
2026-07-07 13:49:46 +0700

    Update listpremium.lua with new entries

--- Main/Premium/listpremium.lua
+++ Main/Premium/listpremium.lua
@@ -1,4 +1,4 @@
--- // NEW LIST | GV-367
+-- // NEW LIST | GV-368
 return {
     -- ====================== Admin ================================
     ["Yolmar_43"] = { Tag = "dyumraisgoodguy", Time = nil, Day = 99999999, Key = "DYHUB-OWNER" },
@@ -153,7 +153,7 @@ return {
     ["zombiearena1020"] = { Tag = "kurothemain", Time = "Lifetime", Day = nil, Key = "DYHUB-SINISTER-fr" },
   
     -- ====================== Buyer (Trial / Short-term) =============================
-    ["explodingtntboy1"] = { Tag = "mongi33", Time = nil, Day = 1, Key = "DYHUB_1d_a9kZa5KFKVj9LpJ4sJHW8cKdFjB6ABNqU" },
+    ["zizipk"] = { Tag = "trucv4421g", Time = nil, Day = 1, Key = "DYHUB_1d_a9kZa5KFKVj9LpJ4sJHW8cKdFjB6ABNqU" },
     ["st4rr0259"] = { Tag = "wralthrblx", Time = nil, Day = 7, Key = "DYHUB_7d_p9kGa5KJKVj9Lpn4sJHW8cpdFLB6ABNqU" },
     ["VtAcak8"] = { Tag = "yuki_muhaha", Time = nil, Day = 150, Key = "DYHUB_150d_e4kRp2MFRBn3SpLfmHhW53" },
     ["Katakuri3797"] = { Tag = "hoilopobk", Time = nil, Day = 3, Key = "DYHUB_3d_e4kRp2MFDBn3SpL4mHhW53" },
