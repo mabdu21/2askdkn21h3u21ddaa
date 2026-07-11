be72836c5eefe22a95648e6c709abfc563fbb782
mabdu21 <woraphan88712345@gmail.com>
2026-07-11 23:32:13 +0700

    Add new entry for del6six6 in listpremium.lua

--- Main/Premium/listpremium.lua
+++ Main/Premium/listpremium.lua
@@ -1,4 +1,4 @@
--- // NEW LIST | GV-379
+-- // NEW LIST | GV-380
 return {
     -- ====================== Admin ================================
     ["Yolmar_43"] = { Tag = "dyumraisgoodguy", Time = nil, Day = 99999999, Key = "DYHUB-OWNER" },
@@ -148,6 +148,7 @@ return {
     ["aerissinia"] = { Tag = "aerissinia", Time = "Lifetime", Day = nil, Key = "DYHUB_lt_n6gPu2MfGTy3IpL4oUhJ53gFpbRt7kVOjU" }, -- today 2026
     ["rekanz22"] = { Tag = "bojessinaga", Time = "Lifetime", Day = nil, Key = "DYHUB_lt_p6gPu2EfGFy3IpL4oUhJ54gFpbRt7uVOjU" }, -- today 2026
     ["ploxxer3"] = { Tag = ".ploxxer", Time = "Lifetime", Day = nil, Key = "DYHUB_lt_q6gPu2EpGFl3IpL4oUhJ52gFpbRt7uJOjU" }, -- today 2026
+    ["del6six6"] = { Tag = "fuzz6six6", Time = "Lifetime", Day = nil, Key = "DYHUB_lt_d6gPu5EpGFl3HpL4oUTJ52gFpbEt7uJOjU" }, -- today 2026
     
     -- ====================== YOUTUBE =============================
     ["Jzyyyyyx"] = { Tag = "growwltoken", Time = "Lifetime", Day = nil, Key = "DYHUB-SINISTER" },
