6085f8db92633353f4d2b939d8161c6a9f784b98
mabdu21 <woraphan88712345@gmail.com>
2026-07-19 20:21:02 +0700

    Fix casing of username 'Abdelali3032'

--- Main/Premium/listpremium.lua
+++ Main/Premium/listpremium.lua
@@ -168,7 +168,7 @@ return {
     ["khian123rc454"] = { Tag = "indraa_65955", Time = nil, Day = 3, Key = "DYHUB_3d_e7uRp2MYDTn3SgL4jPhU53" },
     ["ldbzowv1"] = { Tag = "_supachai.", Time = nil, Day = 7, Key = "DYHUB_7d_e7uRg2SYDEn3SgL4jPhW53" },
     ["Skibidi2025114"] = { Tag = "biasaaj.", Time = nil, Day = 31, Key = "DYHUB_31d_e7uRg2SYDEn3SgL4jPhW53" },
-    ["Abdelali3032"] = { Tag = "abdelali0173_44000", Time = nil, Day = 67, Key = "DYHUB_67d_e7uRg2SYDEn3SgL4jPhW53" },
+    ["abdelali3032"] = { Tag = "abdelali0173_44000", Time = nil, Day = 67, Key = "DYHUB_67d_e7uRg2SYDEn3SgL4jPhW53" },
     
     -- ====================== Booster ==============================
     ["NoraJourneys"] = { Tag = "luzzynobigl", Time = nil, Day = 14, Key = "DYHUB-2GVA5-BOOSTER" },
